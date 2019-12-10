02148fee4105f166c5fdc3b22464687a1b869674
Christopher Hewett <chewett@hotmail.co.uk>
2019-12-10 21:10:07 +0000

    Added day 8/9/10 details at the top of the of the functions

--- src/main/java/net/chewett/adventofcode2019/problems/Day8.java
+++ src/main/java/net/chewett/adventofcode2019/problems/Day8.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 
 
 /**
- *
+ * Taken from: https://adventofcode.com/2019/day/8
  *
  * --- Day 8: Space Image Format ---
  * The Elves' spirits are lifted when they realize you have an opportunity to reboot one of their Mars rovers, and so they are curious if you would spend a brief sojourn on Mars. You land your ship near the rover.
--- src/main/java/net/chewett/adventofcode2019/problems/Day9.java
+++ src/main/java/net/chewett/adventofcode2019/problems/Day9.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Taken from: https://adventofcode.com/2019/day/9
+ *
  * --- Day 9: Sensor Boost ---
  * You've just said goodbye to the rebooted rover and left Mars when you receive a faint distress signal coming from the asteroid belt. It must be the Ceres monitoring station!
  *
