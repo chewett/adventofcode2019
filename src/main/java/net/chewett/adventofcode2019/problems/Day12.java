df23d585de8e046e27a14ce5adb879f28efb4c93
Christopher Hewett <chewett@hotmail.co.uk>
2019-12-12 21:51:10 +0000

    Added Day 12 part one

---
+++ src/main/java/net/chewett/adventofcode2019/problems/Day12.java
@@ -0,0 +1,62 @@
+package net.chewett.adventofcode2019.problems;
+
+import net.chewett.adventofcode2019.Moon;
+import net.chewett.adventofcode2019.PermutationGenerator;
+import net.chewett.adventofcode2019.Position3D;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Day12 {
+
+    public static void printMoons(List<Moon> moons) {
+        int totalEnergy = 0;
+        for(Moon m : moons) {
+            System.out.println("pos=<x=" + m.getPos().getX() + ", y=" +  m.getPos().getY()+", z=" + m.getPos().getZ() + ">,"+
+                    " vel=<x=" + m.getVelocity().getX() + ", y=" + m.getVelocity().getY()  + ", z=" + m.getVelocity().getZ()  + ">");
+            totalEnergy += m.getTotalEnergy();
+        }
+        System.out.println("Total energy: " + totalEnergy);
+    }
+
+    public static void main(String[] args) {
+        int[][] perms = new int[][] {
+            new int[] { 0, 1 },
+            new int[] { 0, 2 },
+            new int[] { 0, 3 },
+            new int[] { 1, 2 },
+            new int[] { 1, 3 },
+            new int[] { 2, 3 }
+        };
+
+
+        List<Moon> moons = new ArrayList<>();
+        moons.add(new Moon(0, 6, 1));
+        moons.add(new Moon(4, 4, 19));
+        moons.add(new Moon(-11, 1,  8));
+        moons.add(new Moon(2, 19, 15));
+
+        Day12.printMoons(moons);
+
+        long steps = 0;
+        while(steps < 1000) {
+            for(int moonPerm = 0; moonPerm < perms.length; moonPerm++) {
+                int[] moonPair = perms[moonPerm];
+                moons.get(moonPair[0]).applyGravity(moons.get(moonPair[1]));
+            }
+            for(Moon m : moons) {
+                m.applyVelocity();
+            }
+            steps++;
+
+            System.out.println("After " + steps + " steps");
+            Day12.printMoons(moons);
+        }
+
+
+
+
+    }
+}
