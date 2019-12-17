691014f5c3c7742f3eb0319122d638b04e110645
Christopher Hewett <chewett@hotmail.co.uk>
2019-12-17 21:10:22 +0000

    Added Day 17

---
+++ src/main/java/net/chewett/adventofcode2019/problems/Day17.java
@@ -0,0 +1,86 @@
+package net.chewett.adventofcode2019.problems;
+
+import net.chewett.adventofcode2019.intcode.Intcode;
+import net.chewett.adventofcode2019.intcode.IntcodeComputer;
+import net.chewett.adventofcode2019.intcode.instructions.*;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Day17 {
+
+    public static void main(String[] args) {
+
+        try {
+            File file = new File("day_17_input.txt");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String oxygenRobot = br.readLine();
+
+            //Set up my Instruction set
+            List<IntcodeInstruction> instructions = new ArrayList<>();
+            instructions.add(new FinishInstruction());
+            instructions.add(new AddInstruction());
+            instructions.add(new MultiplyInstruction());
+            instructions.add(new InputSaveInstruction());
+            instructions.add(new WriteOutputInstruction());
+            instructions.add(new JumpIfTrueInstruction());
+            instructions.add(new JumpIfFalseInstruction());
+            instructions.add(new LessThanInstruction());
+            instructions.add(new EqualsInstruction());
+            instructions.add(new AdjustRelativeBaseInstruction());
+
+            IntcodeComputer icc = new IntcodeComputer(instructions);
+            Intcode ic = new Intcode(oxygenRobot);
+            icc.initIntcode(ic);
+            icc.runIntcode();
+
+            List<String> rows = new ArrayList<>();
+
+            String curRow = "";
+            while(icc.hasOutputToRead()) {
+                long output = icc.getOutput();
+
+                if(output == 10) {
+                    rows.add(curRow);
+                    curRow = "";
+                }else{
+                    curRow += ((char)output);
+                }
+            }
+
+            int totalCombinations = 0;
+            for(int y = 1; y < rows.size() - 1; y++) {
+                for(int x = 1; x < rows.get(y).length() - 1; x++) {
+                    if(rows.get(y).substring(x, x+1).equals("#")) {
+                        if(
+                                rows.get(y).substring(x-1, x).equals("#") &&
+                                rows.get(y).substring(x+1, x+2).equals("#") &&
+                                rows.get(y-1).substring(x, x+1).equals("#") &&
+                                rows.get(y+1).substring(x, x+1).equals("#")
+                        ){
+                            totalCombinations += x * y;
+                        }
+                    }
+                }
+            }
+
+            System.out.println("Total combinations: " + totalCombinations);
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
